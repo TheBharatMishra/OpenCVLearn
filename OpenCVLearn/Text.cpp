51339d2edd8bf121ddc9689b2000775b23fd896f
TheBharatMishra <mishrabm2004@gmail.com>
2023-10-06 12:12:01 +0530

    Add project files.

---
+++ OpenCVLearn/Text.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+
+using namespace std;
+using namespace cv;
+
+
+int main() {
+
+	string path = "Resources/test.png";
+	Mat img = imread(path);
+		imshow("Image", img);
+		waitKey(0);
+
+
+	return 0;
+}
